eb96d00fd12ec428cffe5b035309a8bce96ca60c
ShaderFax <westleyhowe@hotmail.com>
2024-09-06 14:13:34 -0600

    finished

--- Build_A_JS_Calculator/JS-calc/src/App.tsx
+++ Build_A_JS_Calculator/JS-calc/src/App.tsx
@@ -3,17 +3,16 @@ import './App.css';
 
 function App() {
   const [answer, setAnswer] = useState("0");
-  const [expression, setExpression] = useState("0");
-
-  const isOperator = (symbol: string) => {
-    return /[*/+-]/.test(symbol);
-  }
-
+  const [expression, setExpression] = useState("");
   const et = expression.trim();
+  
+  const isOperator = (symbol: string) => {
+    return /[*/+-]/g.test(symbol);
+  };
 
   const buttonPress = (symbol: string) => {
     if (symbol === "clear") {
-      setAnswer("0");
+      setAnswer("");
       setExpression("0");
       } else if (symbol === "negative") {
         if(answer === "") return;
@@ -23,7 +22,53 @@ function App() {
         setAnswer((parseFloat(answer) / 100).toString());
       } else if (isOperator(symbol)){
         setExpression(et + " " + symbol + " ");
+      } else if(symbol === "=") {
+        calculate();
+      } else if(symbol === "0") {
+        if(expression.charAt(0) !== "0") {
+          setExpression(expression + symbol)
+        }
+      } else if(symbol === ".") {
+        const lastNumber = expression.split(/[*/+-]/g).pop();
+        if(lastNumber?.includes(".")) return;
+        setExpression(expression + symbol);
+      } else {
+        if(expression.charAt(0) === "0") {
+          setExpression(expression.slice(1) + symbol);
+        } else {
+          setExpression(expression + symbol);
+        }
       }
+    }
+
+    const calculate = () => {
+      if(isOperator(et.charAt(et.length - 1))) return;
+
+      const parts = et.split(" "); 
+      const newParts = [];
+
+      for (let i = parts.length; i >= 0; i--) {
+        if(["*", "/", "+"].includes(parts[i]) && isOperator(parts[i-1])) {
+          newParts.unshift(parts[i]);
+          let j = 0;
+          let k = i - 1;
+          while(isOperator(parts[k])) {
+            k--;
+            j++;
+          }
+          i -= j;
+        } else {
+          newParts.unshift(parts[i]);
+        }
+      }
+      const newExpression = newParts.join(" "); 
+
+      if(isOperator(newExpression.charAt(0))) {
+        setAnswer(eval(answer + newExpression) as string);
+      } else {
+        setAnswer(eval(newExpression) as string);
+      }
+      setExpression("");
     }
 
   return (
