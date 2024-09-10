42b252d59ff49bd1c83c65972c59910fc9d8e68b
shaderfax <westleyhowe@hotmail.com>
2024-09-09 21:48:20 -0600

    Believe all elements accounted for

--- Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
+++ Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
@@ -8,15 +8,17 @@ function App() {
       <textarea id="editor"></textarea>
       <div id="preview"></div>
       <div id="break-label">Break Length</div>
-      <input type="button" id="break increment"></input>
-      <input type="button" id="break decrement"></input>
+      <input type="button" id="break-increment"></input>
+      <input type="button" id="break-decrement"></input>
       <div id="session-label">Session Length</div>
-      <input type="button" id="session increment"></input>
-      <input type="button" id="session decrement"></input>
+      <input type="button" id="session-increment"></input>
+      <input type="button" id="session-decrement"></input>
       <div id="timer-label">Session</div>
       <div id="session-length">25</div>
-
       <div id="break-length">5</div>
+      <div id="time-left"></div>
+      <input type="button" id="start_stop"></input>
+      <input type="button" id="reset"></input>
     </>
   )
 }
