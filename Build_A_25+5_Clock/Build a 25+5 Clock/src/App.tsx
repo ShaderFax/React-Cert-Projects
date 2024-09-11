be6bb8973c0c2e06690c989e431bd1f761a3f774
ShaderFax <westleyhowe@hotmail.com>
2024-09-11 11:51:27 -0600

    God is dead and we have killed him

--- Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
+++ Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
@@ -1,24 +1,28 @@
 import { useState } from "react"
 import './App.css'
+import './Alarm'
+import './Timer'
 
 function App() {
+  const [state, setCount] = { useState };
 
   return (
     <>
       <textarea id="editor"></textarea>
       <div id="preview"></div>
-      <div id="break-label">Break Length</div>
-      <input type="button" id="break-increment"></input>
-      <input type="button" id="break-decrement"></input>
-      <div id="session-label">Session Length</div>
-      <input type="button" id="session-increment"></input>
-      <input type="button" id="session-decrement"></input>
-      <div id="timer-label">Session</div>
       <div id="session-length">25</div>
       <div id="break-length">5</div>
+      <div id="break-label">Break Length</div>
+      <button type="button" id="break-increment">+</button>
+      <button type="button" id="break-decrement">-</button>
+      <div id="session-label">Session Length</div>
+      <button type="button" id="session-increment">+</button>
+      <button type="button" id="session-decrement">-</button>
+      <div id="timer-label">Session</div>
+      
       <div id="time-left"></div>
-      <input type="button" id="start_stop"></input>
-      <input type="button" id="reset"></input>
+      <button type="button" id="start_stop">Start</button>
+      <button type="button" id="reset">Reset</button>
     </>
   )
 }
