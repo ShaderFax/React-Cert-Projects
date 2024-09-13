53a3a66a9c8d100b073e9447dd8fa4df77f08d71
ShaderFax <westleyhowe@hotmail.com>
2024-09-13 10:37:49 -0600

    progress

--- Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
+++ Build_A_25+5_Clock/Build a 25+5 Clock/src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react"
 import './App.css'
-import './Alarm'
-import './Timer'
+
+const defaultBreakTime = 5 * 60;
+const defaultSessionTime = 25 * 60;
+const min = 60;
+const max = 60 * 60;
+const interval = 60;
 
 function App() {
-  const [state, setCount] = { useState };
+  const [breakTime, setBreakTime] = useState(defaultBreakTime);
+  const [sessionTime, setSessionTime] = useState(defaultSessionTime);
+  const [displayState, setDisplayState] = useState<DisplayState>("session");
+
 
   return (
     <>
