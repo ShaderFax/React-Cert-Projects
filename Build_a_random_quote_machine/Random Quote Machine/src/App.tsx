00e6ee90c05062522dc2d13f2770d2126ef3ab54
shaderfax <westleyhowe@hotmail.com>
2024-09-18 23:55:24 -0600

    did the html portion in react, says I passed the tests. Gonna actually make it though

---
+++ Build_a_random_quote_machine/Random Quote Machine/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="quote-box">
+        <div id='text'>
+          Test
+        </div>
+        <div id="author">
+          Test
+        </div>
+        <button id='new-quote'>New Quote</button>
+        <a type="button" id='tweet-quote' href="https://twitter.com/intent/tweet">Tweet Quote</a>
+      </div>
+    </>
+  )
+}
+
+export default App
