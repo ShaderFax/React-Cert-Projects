0f1073825e04f4610377783f0d4636f95de28b69
shaderfax <westleyhowe@hotmail.com>
2024-09-24 21:55:01 -0600

    4/8 tests pass

--- Build_a_Markdown_Previewer/Markdown previewer/src/App.tsx
+++ Build_a_Markdown_Previewer/Markdown previewer/src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [edit, setEdit] = useState("");
 
   return (
     <>
-      
+      <textarea id="editor" placeholder="Enter text here." 
+      onChange={e => setEdit(e.target.value)}>
+
+      </textarea>
+      <div id="preview">
+        {edit}
+      </div>
     </>
   )
 }
