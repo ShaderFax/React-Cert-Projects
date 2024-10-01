1945b907f585437fb28d319c23fb2976db537744
shaderfax <westleyhowe@hotmail.com>
2024-09-30 22:09:30 -0600

    5/8 tests successful

--- Build_a_Drum_Machine/Drum Machine/src/App.tsx
+++ Build_a_Drum_Machine/Drum Machine/src/App.tsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
   return (
     <>
-      
+      <div id="drum-machine">
+        <div id="display">
+
+        </div>
+        <button className="drum-pad" id="drum-1"><audio src="./assets/clip.mp3" className="clip" id='Q' />Q</button>
+        <button className="drum-pad" id="drum-2"><audio src="./assets/clip.mp3" className="clip" id='W' />W</button>
+        <button className="drum-pad" id="drum-3"><audio src="./assets/clip.mp3" className="clip" id='E' />E</button>
+        <button className="drum-pad" id="drum-4"><audio src="./assets/clip.mp3" className="clip" id='A' />A</button>
+        <button className="drum-pad" id="drum-5"><audio src="./assets/clip.mp3" className="clip" id='S' />S</button>
+        <button className="drum-pad" id="drum-6"><audio src="./assets/clip.mp3" className="clip" id='D' />D</button>
+        <button className="drum-pad" id="drum-7"><audio src="./assets/clip.mp3" className="clip" id='Z' />Z</button>
+        <button className="drum-pad" id="drum-8"><audio src="./assets/clip.mp3" className="clip" id='X' />X</button>
+        <button className="drum-pad" id="drum-9"><audio src="./assets/clip.mp3" className="clip" id='C' />C</button>
+      </div>
     </>
   )
 }
