a79b2f8a837cff9026b27b059856a67a5c54c1ae
shaderfax <westleyhowe@hotmail.com>
2024-10-08 21:54:25 -0600

    done

--- Build_a_Drum_Machine/Drum Machine/src/App.tsx
+++ Build_a_Drum_Machine/Drum Machine/src/App.tsx
@@ -1,24 +1,77 @@
-import { useState } from 'react'
 import './App.css'
-import Sound from './assets/clip.mp3';
+import { AudioClip } from './types';
+import Drum from './assets/drum';
+
+const audioClips: AudioClip[] = [
+  {
+    keyTrigger: "Q",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    description: "Heater 1",
+  },
+  {
+    keyTrigger: "W",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    description: "Heater 2",
+  },
+  {
+    keyTrigger: "E",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    description: "Heater 3",
+  },
+  {
+    keyTrigger: "A",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    description: "Heater 4",
+  },
+  {
+    keyTrigger: "S",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    description: "Clap",
+  },
+  {
+    keyTrigger: "D",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    description: "Open HH",
+  },
+  {
+    keyTrigger: "Z",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    description: "Kick n' Hat",
+  },
+  {
+    keyTrigger: "X",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    description: "Kick",
+  },
+  {
+    keyTrigger: "C",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    description: "Closed HH",
+  },
+];
 
 function App(){
 
+  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const clip = audioClips.find(
+      (clip) => clip.keyTrigger === e.key.toUpperCase()
+    );
+    if(!clip) return;
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement).play().catch(console.error);
+    document.getElementById("drum-" + clip.keyTrigger)?.focus();
+    document.getElementById("display")!.innerText = clip.description;
+  };
+
   return (
     <>
-      <div id="drum-machine">
-        <div id="display">
-          
+      <div className='container' id="drum-machine" onKeyDown={playAudio}>
+        <h1>FCC Drum Machine</h1>
+        <div className='whole-drum'>
+          {audioClips.map((clip) => (
+            <Drum audioClip={clip} key={clip.keyTrigger} />
+          ))}
         </div>
-        <button className="drum-pad" id="drum-1"><audio src={Sound} className="clip" id='Q' />Q</button>
-        <button className="drum-pad" id="drum-2"><audio src={Sound} className="clip" id='W' />W</button>
-        <button className="drum-pad" id="drum-3"><audio src={Sound} className="clip" id='E' />E</button>
-        <button className="drum-pad" id="drum-4"><audio src={Sound} className="clip" id='A' />A</button>
-        <button className="drum-pad" id="drum-5"><audio src={Sound} className="clip" id='S' />S</button>
-        <button className="drum-pad" id="drum-6"><audio src={Sound} className="clip" id='D' />D</button>
-        <button className="drum-pad" id="drum-7"><audio src={Sound} className="clip" id='Z' />Z</button>
-        <button className="drum-pad" id="drum-8"><audio src={Sound} className="clip" id='X' />X</button>
-        <button className="drum-pad" id="drum-9"><audio src={Sound} className="clip" id='C' />C</button>
+        <div id="display"></div>
       </div>
     </>
   )
